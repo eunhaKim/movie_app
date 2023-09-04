a38ef9f1b29f8a999c79708031a6975c09f50714
eunhaKim <ara419@naver.com>
2023-09-04 12:48:01 +0900

    20230904

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
-function App(){
-  return <div>Hello!!!!</div>
-}
+
+function App() {
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Routes>
+        <Route path="/" exact={true} Component={Home}/>
+        <Route path="/about" Component={About}/>
+        <Route path="/movie-detail" Component={Detail} />
+      </Routes>
+    </HashRouter>
+  );
+};
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Detail(props) {
+  console.log(props);
+  return (
+    <span>Detail</span>
+  )
+}
+
+export default Detail
